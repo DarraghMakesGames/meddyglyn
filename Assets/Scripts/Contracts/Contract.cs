d7bed7790207c091df3e6cb1cd591aeb7672e90b
DarraghMakesGames <D20123637@mytudublin.ie>
2021-10-26 12:28:43 +0100

    Created scriptableobject template for contracts

---
+++ Assets/Scripts/Contracts/Contract.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Contract", menuName = "Contract")]
+public class Contract : ScriptableObject
+{
+
+    public string contractName;
+    public string description;
+
+    public float targetVolume;
+    public float targetSweetness;
+    public float targetCitrus;
+    public float targetTart;
+    public float targetSour;
+    public float targetBitter;
+    public float targetWoody;
+    public float targetPeppery;
+    public float targetAge;
+    public float targetAlcohol;
+
+}

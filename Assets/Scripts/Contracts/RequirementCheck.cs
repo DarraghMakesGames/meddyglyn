464a76f22b822f7bf88577f0159c9e3253af1e35
DarraghMakesGames <D20123637@mytudublin.ie>
2021-10-31 17:11:02 +0000

    Updated requirement checker to generate strings based on incorrect values

--- Assets/Scripts/Contracts/RequirementCheck.cs
+++ Assets/Scripts/Contracts/RequirementCheck.cs
@@ -60,6 +60,31 @@ public class RequirementCheck : MonoBehaviour
     public bool matchesAge = false;
     public bool matchesAlcohol = false;
 
+    [SerializeField] private string lackVolume;
+    [SerializeField] private string lackSweetness;
+    [SerializeField] private string overSweetness;
+    [SerializeField] private string lackCitrus;
+    [SerializeField] private string overCitrus;
+    [SerializeField] private string lackTart;
+    [SerializeField] private string overTart;
+    [SerializeField] private string lackSour;
+    [SerializeField] private string overSour;
+    [SerializeField] private string lackBitter;
+    [SerializeField] private string overBitter;
+    [SerializeField] private string lackWoody;
+    [SerializeField] private string overWoody;
+    [SerializeField] private string lackPeppery;
+    [SerializeField] private string overPeppery;
+    [SerializeField] private string lackFloral;
+    [SerializeField] private string overFloral;
+    [SerializeField] private string lackEarthy;
+    [SerializeField] private string overEarthy;
+    [SerializeField] private string lackGreen;
+    [SerializeField] private string overGreen;
+    [SerializeField] private string lackAge;
+    [SerializeField] private string lackAlcohol;
+    [SerializeField] private string overAlcohol;
+
 
     void Update()
     {
@@ -126,118 +151,233 @@ public class RequirementCheck : MonoBehaviour
         if (actualVolume >= targetVolume)
         {
             matchesVolume = true;
+            lackVolume = null;
         }
         else
         {
             matchesVolume = false;
+            lackVolume = " You didn't make enough! ";
         }
 
         if (actualAge >= targetMinAge)
         {
             matchesAge = true;
+            lackAge = null;
         }
         else
         {
             matchesAge = false;
+            lackAge = " This wasn't aged for long enough. ";
         }
+
 
         if (actualSweetness >= targetMinSweetness && actualSweetness <= targetMaxSweetness)
         {
             matchesSweetness = true;
+            lackSweetness = null;
+            overSweetness = null;
         }
-        else
+        else if (actualSweetness < targetMinSweetness)
         {
             matchesSweetness = false;
+            overSweetness = null;
+            lackSweetness = " It isn't sweet enough. ";
+        }
+        else if (actualSweetness > targetMaxSweetness)
+        {
+            matchesSweetness = false;
+            lackSweetness = null;
+            overSweetness = " It's too sweet. ";
         }
 
         if (actualAlcohol >= targetMinAlcohol && actualAlcohol <= targetMaxAlcohol)
         {
             matchesAlcohol = true;
+            lackAlcohol = null;
+            overAlcohol = null;
         }
-        else
+        else if (actualAlcohol < targetMinAlcohol)
         {
             matchesAlcohol = false;
+            lackAlcohol = " The alcohol content isn't high enough. ";
+            overAlcohol = null;
+        }
+        else if (actualAlcohol > targetMaxAlcohol)
+        {
+            matchesAlcohol = false;
+            lackAlcohol = null;
+            overAlcohol = " The alcohol content is unpleasantly high. ";
         }
 
         if (actualCitrus >= targetMinCitrus && actualCitrus <= targetMaxCitrus)
         {
             matchesCitrus = true;
+            lackCitrus = null;
+            overCitrus = null;
         }
-        else
+        else if (actualCitrus < targetMinCitrus)
         {
             matchesCitrus = false;
+            overCitrus = null;
+            lackCitrus = " There's not enough Citrus flavour. ";
+        }
+        else if (actualCitrus > targetMaxCitrus)
+        {
+            matchesCitrus = false;
+            lackCitrus = null;
+            overCitrus = " The Citrus flavour is too strong. ";
         }
 
         if (actualTart >= targetMinTart && actualTart <= targetMaxTart)
         {
             matchesTart = true;
+            lackTart = null;
+            overTart = null;
         }
-        else
+        else if (actualTart < targetMinTart)
         {
             matchesTart = false;
+            overTart = null;
+            lackTart = " There's not enough Tart flavour. ";
+        }
+        else if (actualTart > targetMaxTart)
+        {
+            matchesTart = false;
+            lackTart = null;
+            overTart = " The Tart flavour is too strong. ";
         }
 
         if (actualSour >= targetMinSour && actualSour <= targetMaxSour)
         {
             matchesSour = true;
+            lackSour = null;
+            overSour = null;
         }
-        else
+        else if (actualSour < targetMinSour)
         {
             matchesSour = false;
+            overSour = null;
+            lackSour = " There's not enough Sour flavour. ";
+        }
+        else if (actualSour > targetMaxSour)
+        {
+            matchesSour = false;
+            lackSour = null;
+            overSour = " The Sour flavour is too strong. ";
         }
 
         if (actualBitter >= targetMinBitter && actualBitter <= targetMaxBitter)
         {
             matchesBitter = true;
+            lackBitter = null;
+            overBitter = null;
         }
-        else
+        else if (actualBitter < targetMinBitter)
         {
             matchesBitter = false;
+            overBitter = null;
+            lackBitter = " There's not enough Bitter flavour. ";
+        }
+        else if (actualBitter > targetMaxBitter)
+        {
+            matchesBitter = false;
+            lackBitter = null;
+            overBitter = " The Bitter flavour is too strong. ";
         }
 
         if (actualWoody >= targetMinWoody && actualWoody <= targetMaxWoody)
         {
             matchesWoody = true;
+            lackWoody = null;
+            overWoody = null;
         }
-        else
+        else if (actualWoody < targetMinWoody)
         {
             matchesWoody = false;
+            overWoody = null;
+            lackWoody = " There's not enough Woody flavour. ";
+        }
+        else if (actualWoody > targetMaxWoody)
+        {
+            matchesWoody = false;
+            lackWoody = null;
+            overWoody = " The Woody flavour is too strong. ";
         }
 
         if (actualPeppery >= targetMinPeppery && actualPeppery <= targetMaxPeppery)
         {
             matchesPeppery = true;
+            lackPeppery = null;
+            overPeppery = null;
         }
-        else
+        else if (actualPeppery < targetMinPeppery)
         {
             matchesPeppery = false;
+            overPeppery = null;
+            lackPeppery = " There's not enough Peppery flavour. ";
+        }
+        else if (actualPeppery > targetMaxPeppery)
+        {
+            matchesPeppery = false;
+            lackPeppery = null;
+            overPeppery = " The Peppery flavour is too strong. ";
         }
 
         if (actualFloral >= targetMinFloral && actualFloral <= targetMaxFloral)
         {
             matchesFloral = true;
+            lackFloral = null;
+            overFloral = null;
         }
-        else
+        else if (actualFloral < targetMinFloral)
         {
             matchesFloral = false;
+            overFloral = null;
+            lackFloral = " There's not enough Floral flavour. ";
+        }
+        else if (actualFloral > targetMaxFloral)
+        {
+            matchesFloral = false;
+            lackFloral = null;
+            overFloral = " The Floral flavour is too strong. ";
         }
 
         if (actualEarthy >= targetMinEarthy && actualEarthy <= targetMaxEarthy)
         {
             matchesEarthy = true;
+            lackEarthy = null;
+            overEarthy = null;
         }
-        else
+        else if (actualEarthy < targetMinEarthy)
         {
             matchesEarthy = false;
+            overEarthy = null;
+            lackEarthy = " There's not enough Earthy flavour. ";
+        }
+        else if (actualEarthy > targetMaxEarthy)
+        {
+            matchesEarthy = false;
+            lackEarthy = null;
+            overEarthy = " The Earthy flavour is too strong. ";
         }
 
         if (actualGreen >= targetMinGreen && actualGreen <= targetMaxGreen)
         {
             matchesGreen = true;
+            lackGreen = null;
+            overGreen = null;
         }
-        else
+        else if (actualGreen < targetMinGreen)
         {
             matchesGreen = false;
+            overGreen = null;
+            lackGreen = " There's not enough Green flavour. ";
+        }
+        else if (actualGreen > targetMaxGreen)
+        {
+            matchesGreen = false;
+            lackGreen = null;
+            overGreen = " The Green flavour is too strong. ";
         }
 
 

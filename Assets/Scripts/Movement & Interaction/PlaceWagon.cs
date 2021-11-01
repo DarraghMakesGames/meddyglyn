dd7b26af5ff5b488f29299469aa376e811db4ded
DarraghMakesGames <D20123637@mytudublin.ie>
2021-11-01 10:43:28 +0000

    Added storage positions to contract wagon & script to populate each position in order when a player places a storage object

---
+++ Assets/Scripts/Movement & Interaction/PlaceWagon.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceWagon : MonoBehaviour
+{
+
+    private GameObject CarriedObject;
+    private GameObject carriedObj;
+    private Transform carried;
+
+    [SerializeField] Transform pos1;
+    [SerializeField] Transform pos2;
+    [SerializeField] Transform pos3;
+    [SerializeField] Transform pos4;
+    [SerializeField] Transform pos5;
+    [SerializeField] Transform pos6;
+    [SerializeField] Transform pos7;
+    [SerializeField] Transform pos8;
+
+    private Transform placedPosition;
+    private Vector3 placedPos;
+
+    private bool hasItem = false;
+
+    private void Start()
+    {
+        CarriedObject = GameObject.Find("CarriedObject");
+
+    }
+
+
+    public void Interacted()
+    {
+        if (CarriedObject.transform.childCount < 1)
+        {
+            Debug.Log("Not carrying anything");
+        }
+        else if (!hasItem)
+        {
+            carried = CarriedObject.gameObject.transform.GetChild(0);
+            carriedObj = carried.transform.gameObject;
+
+            if (carriedObj.gameObject.CompareTag("Ingredient"))
+            {
+                Debug.Log("Only accepts finished brews");
+            }
+
+            else if (carriedObj.gameObject.CompareTag("Storage"))
+            {
+                Debug.Log("Adding brew");
+                if (pos1.gameObject.transform.childCount <= 0)
+                {
+                    carried.position = pos1.position;
+                    carried.rotation = Quaternion.identity;
+                    carried.transform.parent = pos1;
+                    CarriedObject.GetComponent<CarryingManager>().playPlaceSound();
+                }
+                else if (pos2.gameObject.transform.childCount <= 0)
+                {
+                    carried.position = pos2.position;
+                    carried.rotation = Quaternion.identity;
+                    carried.transform.parent = pos2;
+                    CarriedObject.GetComponent<CarryingManager>().playPlaceSound();
+                }
+                else if (pos3.gameObject.transform.childCount <= 0)
+                {
+                    carried.position = pos3.position;
+                    carried.rotation = Quaternion.identity;
+                    carried.transform.parent = pos3;
+                    CarriedObject.GetComponent<CarryingManager>().playPlaceSound();
+                }
+                else if (pos4.gameObject.transform.childCount <= 0)
+                {
+                    carried.position = pos4.position;
+                    carried.rotation = Quaternion.identity;
+                    carried.transform.parent = pos4;
+                    CarriedObject.GetComponent<CarryingManager>().playPlaceSound();
+                }
+                else if (pos5.gameObject.transform.childCount <= 0)
+                {
+                    carried.position = pos5.position;
+                    carried.rotation = Quaternion.identity;
+                    carried.transform.parent = pos5;
+                    CarriedObject.GetComponent<CarryingManager>().playPlaceSound();
+                }
+                else if (pos6.gameObject.transform.childCount <= 0)
+                {
+                    carried.position = pos6.position;
+                    carried.rotation = Quaternion.identity;
+                    carried.transform.parent = pos6;
+                    CarriedObject.GetComponent<CarryingManager>().playPlaceSound();
+                }
+                else if (pos7.gameObject.transform.childCount <= 0)
+                {
+                    carried.position = pos7.position;
+                    carried.rotation = Quaternion.identity;
+                    carried.transform.parent = pos7;
+                    CarriedObject.GetComponent<CarryingManager>().playPlaceSound();
+                }
+                else if (pos8.gameObject.transform.childCount <= 0)
+                {
+                    carried.position = pos8.position;
+                    carried.rotation = Quaternion.identity;
+                    carried.transform.parent = pos8;
+                    CarriedObject.GetComponent<CarryingManager>().playPlaceSound();
+                }
+                else
+                {
+                    Debug.Log("No room on wagon");
+                }
+
+
+            }
+        }
+
+
+
+
+        else
+        {
+            Debug.Log("No room for this");
+        }
+    }
+
+}

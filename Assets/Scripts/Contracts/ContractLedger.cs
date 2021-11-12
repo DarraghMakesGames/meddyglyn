49451ad08011089d4820c4a194754d4805972134
DarraghMakesGames <D20123637@mytudublin.ie>
2021-11-12 15:09:55 +0000

    Added clickable button to contract ledger UI

---
+++ Assets/Scripts/Contracts/ContractLedger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ContractLedger : MonoBehaviour
+{
+
+    [SerializeField] private Button contract1Button;
+    [SerializeField] private Button contract2Button;
+    [SerializeField] private Button contract3Button;
+    [SerializeField] private Button activeButton;
+
+    private void Start()
+    {
+       // contract1Button.interactable = false;
+    }
+
+}

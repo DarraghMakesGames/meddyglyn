89504146abc3bb09d953d48e1fe1ca0f7c812ed2
DarraghMakesGames <D20123637@mytudublin.ie>
2021-11-12 15:20:26 +0000

    Added 2 more buttons, each one disables itself and enables others on press

--- Assets/Scripts/Contracts/ContractLedger.cs
+++ Assets/Scripts/Contracts/ContractLedger.cs
@@ -10,11 +10,27 @@ public class ContractLedger : MonoBehaviour
     [SerializeField] private Button contract1Button;
     [SerializeField] private Button contract2Button;
     [SerializeField] private Button contract3Button;
-    [SerializeField] private Button activeButton;
 
-    private void Start()
+    public void ShowContract1()
     {
-       // contract1Button.interactable = false;
+        contract1Button.interactable = false;
+        contract2Button.interactable = true;
+        contract3Button.interactable = true;
     }
+
+    public void ShowContract2()
+    {
+        contract1Button.interactable = true;
+        contract2Button.interactable = false;
+        contract3Button.interactable = true;
+    }
+
+    public void ShowContract3()
+    {
+        contract1Button.interactable = true;
+        contract2Button.interactable = true;
+        contract3Button.interactable = false;
+    }
+
 
 }
